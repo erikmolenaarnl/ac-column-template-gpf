891914a156a5a0de202cfe623c81c8fba61bde8f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-11-19 17:15:57 +0100

    Separate method for validation exclude status

--- ac-column-gpf.php
+++ ac-column-gpf.php
@@ -19,6 +19,37 @@ class AC_Column_gpf extends \AC\Column\Meta {
 		return 'gpf_priority_level';
 	}
 
+	/**
+	 * The meta key for the woocommerce GPF excluded data.
+	 * @return string
+	 */
+	public function get_woocommerce_gpf_key() {
+		return '_woocommerce_gpf_data';
+	}
+
+	/**
+	 * Check if a post is excluded from the product feed.
+	 */
+	public function product_is_excluded( $post_id ) {
+
+		// Retrieving the serialized data
+		$gpf_serialized = get_post_meta ( $post_id, $this->get_woocommerce_gpf_key(), true );
+
+		// Checking if custom field was found. If not, die.
+		// Since this is an array type field (serialized) it should always return an array.
+		if ( ! is_array( $gpf_serialized ) ) {
+			return true;
+		}
+
+		// Checking if 'excluded_product' exists in array AND if it is set to 'on'
+		if ( isset ( $gpf_serialized['exclude_product'] ) && $gpf_serialized['exclude_product'] === 'on' ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
 	 * Get the raw, underlying value for the column
 	 * Not suitable for direct display, use get_value() for that
 	 * This value will be used by 'inline-edit' and get_value().
@@ -32,44 +63,18 @@ class AC_Column_gpf extends \AC\Column\Meta {
 		// put all the column logic here to retrieve the value you need
 		// For example: $value = get_post_meta ( $post_id, '_my_custom_field_example', true );
 
-		// Retrieving the serialized data
-		$gpf_serialized = get_post_meta ( $post_id, '_woocommerce_gpf_data', true );
-
-		// Checking if custom field was found for this post ID. If not, die.
-		if ( $gpf_serialized === '' ) {
-			return;
+		// Checking if this post is excluded.
+		if ( $this->product_is_excluded( $post_id ) ) {
+			return __( 'Off', 'ac-column-template-gpf' );
 		}
 
-		// Checking if 'excluded_product' exists in array to prevent PHP notice
-		if ( isset ( $gpf_serialized['exclude_product'] ) ) {
+		$value = parent::get_raw_value( $post_id );
 
-			// Checking if 'exclude_product' is set to on. If yes, die.
-			if ( $gpf_serialized['exclude_product'] === 'on' ) {
-
-				return;
-
-			}
-
-
+		if ( ! $value ) {
+			return __( 'On', 'ac-column-template-gpf' );
 		}
 
-		// Get the priority level from the Custom Field
-		if ( get_field ( 'gpf_priority_level' ) ) {
-
-			// Set the priority level
-			$gpf_feed_status = get_field ( 'gpf_priority_level' );
-
-		} else {
-
-			// If no custom field is available for the priority level. Just say 'on' instead.
-			$gpf_feed_status = 'On';
-
-		}
-
-		if ( isset ( $gpf_feed_status ) ) {
-			return $gpf_feed_status;
-		}
-		
+		return $value;
 
 	}
 
