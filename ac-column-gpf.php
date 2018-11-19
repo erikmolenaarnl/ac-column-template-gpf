b74cb496d3c0e9c69ee2ce45865db3ca5b1f2e2b
Erik Molenaar <info@erikmolenaar.nl>
2018-11-19 16:06:04 +0100

    Products were not identified correctly as included in GPF

--- ac-column-gpf.php
+++ ac-column-gpf.php
@@ -28,28 +28,35 @@ class AC_Column_gpf extends \AC\Column {
 		// Retrieving the serialized data
 		$gpf_serialized = get_post_meta ( $post_id, '_woocommerce_gpf_data', true );
 
-		// Checking if custom field was found. If not, die.
-		if ( empty ( $gpf_serialized ) ) {
+		// Checking if custom field was found for this post ID. If not, die.
+		if ( $gpf_serialized === '' ) {
 			return;
 		}
 
-		// Checking if 'excluded_product' exists in array AND  if it is set to 'on'
-		if ( ! ( isset ( $gpf_serialized['exclude_product'] ) &&
-				 $gpf_serialized['exclude_product'] === 'on' ) ) {
-	
-			// Get the priority level from the Custom Field
-			if ( get_field ( 'gpf_priority_level' ) ) {
+		// Checking if 'excluded_product' exists in array to prevent PHP notice
+		if ( isset ( $gpf_serialized['exclude_product'] ) ) {
 
-				// Set the priority level
-				$gpf_feed_status = get_field ( 'gpf_priority_level' );
-	
-			} else {
+			// Checking if 'exclude_product' is set to on. If yes, die.
+			if ( $gpf_serialized['exclude_product'] === 'on' ) {
 
-				// If no custom field is available for the priority level. Just say 'on' instead.
-				$gpf_feed_status = 'On';
+				return;
 
 			}
-		
+
+
+		}
+
+		// Get the priority level from the Custom Field
+		if ( get_field ( 'gpf_priority_level' ) ) {
+
+			// Set the priority level
+			$gpf_feed_status = get_field ( 'gpf_priority_level' );
+
+		} else {
+
+			// If no custom field is available for the priority level. Just say 'on' instead.
+			$gpf_feed_status = 'On';
+
 		}
 
 		if ( isset ( $gpf_feed_status ) ) {
